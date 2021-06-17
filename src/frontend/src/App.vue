fb48c59d37062a8d68afc0aa8467cc79cde7249a
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-06-18 00:20:36 +0900

    作業中

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <header>
-        <v-app-bar app color="primary" dark>
-          <v-toolbar-title>Fast Posting</v-toolbar-title>
-        </v-app-bar>
-      </header>
-    </v-app-bar>
+    <v-app-bar app color="primary" dark> </v-app-bar>
 
     <v-main>
       <layout />
