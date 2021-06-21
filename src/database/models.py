7fe2cd54ff53cf148fda6df0b8b6cb7f57069feb
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-06-21 13:40:13 +0900

    userにimage,memo登録されるの確認

--- src/database/models.py
+++ src/database/models.py
@@ -1,7 +1,7 @@
 import sys
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float, DateTime, Text
-from config import Base, ENGINE, TEST_ENGINE
+from .config import Base, ENGINE, TEST_ENGINE
 from datetime import datetime
 
 
